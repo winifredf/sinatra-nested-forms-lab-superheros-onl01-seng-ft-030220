5462ac34773097d1ce7f9de5047c24ed6413bf43
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-18 14:56:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
       @team_motto = (params["team"]["motto"])
       @team_members = (params[":team"][":members"])
       
-      erb :'/team'
+      erb :'..views/team'
     end
   
 
